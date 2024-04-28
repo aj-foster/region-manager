7a615325cba56898f9b7ea56cd113ff449365a48
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2024-04-28 18:35:20 -0400

    Do not disable local adapter in production (during testing)

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ config :rm, RMWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.j
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: RM.Finch
 
 # Disable Swoosh Local Memory Storage
-config :swoosh, local: false
+# config :swoosh, local: false
 
 # Do not print debug messages in production
 config :logger, level: :info
