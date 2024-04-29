25338d705d1334e62d49cb2dfd407503b29348ad
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2024-04-29 00:17:27 -0400

    Add placeholder team list for leagues

--- lib/rm_web/live/league_live/teams.ex
+++ lib/rm_web/live/league_live/teams.ex
@@ -6,7 +6,14 @@ defmodule RMWeb.LeagueLive.Teams do
   on_mount {RMWeb.LeagueLive.Util, :require_league_manager}
 
   def mount(_params, _session, socket) do
+    league = socket.assigns[:league]
+    unready_teams = Enum.reject(league.teams, & &1.event_ready)
+
     socket
+    |> assign(
+      unready_teams: unready_teams,
+      unready_team_count: length(unready_teams)
+    )
     |> ok()
   end
 end
