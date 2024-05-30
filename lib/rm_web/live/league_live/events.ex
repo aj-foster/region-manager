bad079ef5be047ef9dfd40536677a05df31471c7
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2024-05-30 02:07:29 -0400

    Add event metadata to league event listing

--- lib/rm_web/live/league_live/events.ex
+++ lib/rm_web/live/league_live/events.ex
@@ -2,6 +2,8 @@ defmodule RMWeb.LeagueLive.Events do
   use RMWeb, :live_view
   import RMWeb.LeagueLive.Util
 
+  alias RM.FIRST.Event
+
   on_mount {RMWeb.LeagueLive.Util, :preload_league}
   on_mount {RMWeb.LeagueLive.Util, :require_league_manager}
 
