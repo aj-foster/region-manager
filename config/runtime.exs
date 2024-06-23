e7ecb12b0319e816f9960d644ae93b9cf664f2c1
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2024-06-23 14:39:14 -0400

    Fix SSL opts for database

--- config/runtime.exs
+++ config/runtime.exs
@@ -23,8 +23,7 @@ if config_env() == :prod do
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6,
-    ssl: true,
-    ssl_opts: [
+    ssl: [
       verify: :verify_peer,
       cacerts: cacerts,
       server_name_indication: :disable
