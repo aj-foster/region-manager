f6bb1167b1c3c7289b596ecb8cbd84bfe7d2ecc4
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2024-09-19 02:08:01 -0400

    Move past events on team events page

--- lib/rm_web/live/team_live/events.ex
+++ lib/rm_web/live/team_live/events.ex
@@ -27,12 +27,13 @@ defmodule RMWeb.TeamLive.Events do
     team = socket.assigns[:team]
     registered_event_ids = Enum.map(team.event_registrations, & &1.event_id)
 
-    assign_async(socket, :eligible_events, fn ->
+    assign_async(socket, [:eligible_events, :past_events], fn ->
       events =
         RM.FIRST.list_eligible_events_by_team(team, season: team.region.current_season)
         |> Enum.reject(&(&1.id in registered_event_ids))
 
-      {:ok, %{eligible_events: events}}
+      {past_events, upcoming_events} = Enum.split_with(events, &RM.FIRST.Event.event_passed?/1)
+      {:ok, %{eligible_events: upcoming_events, past_events: past_events}}
     end)
   end
 end
