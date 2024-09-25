e8143f1a3282be0918cef53e346c5891e7b2c8ac
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2024-09-25 00:05:49 -0400

    Create email address records for all known addresses

---
+++ lib/rm/account/email.ex
@@ -0,0 +1,73 @@
+defmodule RM.Account.Email do
+  @moduledoc """
+  Record of an email address known to Region Manager
+
+  This record, and the corresponding protections that use it, was added in response to a large
+  number of fake user registrations that caused email confirmations to be sent as spam. While we
+  obviously don't want to contribute spam to the world, we also need to protect the sending
+  reputation of this application.
+
+  This record also tracks if a known email address starts to bounce messages.
+  """
+  use Ecto.Schema
+  import Ecto.Query
+
+  alias Ecto.Changeset
+
+  @typedoc "Email address known to Region Manager"
+  @type t :: %__MODULE__{
+          bounce_count: integer,
+          email: String.t(),
+          first_bounced_at: DateTime.t() | nil,
+          id: Ecto.UUID.t(),
+          inserted_at: DateTime.t(),
+          last_bounced_at: DateTime.t() | nil,
+          updated_at: DateTime.t()
+        }
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  schema "rm_emails" do
+    field :email, :string
+
+    field :bounce_count, :integer
+    field :first_bounced_at, :utc_datetime_usec
+    field :last_bounced_at, :utc_datetime_usec
+
+    timestamps type: :utc_datetime_usec
+  end
+
+  #
+  # Changesets
+  #
+
+  @doc """
+  Prepare one changeset or many parameter maps with new email address records
+  """
+  @spec new([String.t()]) :: [map]
+  def new(emails) when is_list(emails) do
+    now = DateTime.utc_now()
+
+    for email <- emails do
+      %{email: String.downcase(email), inserted_at: now, updated_at: now}
+    end
+  end
+
+  @spec new(String.t()) :: Changeset.t(t)
+  def new(email) do
+    %__MODULE__{}
+    |> Changeset.change(email: String.downcase(email))
+  end
+
+  #
+  # Queries
+  #
+
+  @doc """
+  Query to find an email record by the address
+  """
+  @spec by_email_query(String.t()) :: Ecto.Query.t()
+  def by_email_query(email) do
+    from(__MODULE__, as: :email)
+    |> where([email: e], e.email == ^email)
+  end
+end
---
+++ priv/repo/migrations/20240925005735_create_rm_emails.exs
@@ -0,0 +1,38 @@
+defmodule RM.Repo.Migrations.CreateRmEmails do
+  use Ecto.Migration
+
+  def change do
+    create_if_not_exists table(:rm_emails, primary_key: false) do
+      add :id, :uuid, primary_key: true
+
+      add :email, :citext, null: false
+
+      add :bounce_count, :integer, null: false, default: 0
+      add :first_bounced_at, :utc_datetime_usec
+      add :last_bounced_at, :utc_datetime_usec
+
+      timestamps type: :utc_datetime_usec
+    end
+
+    create_if_not_exists unique_index(:rm_emails, [:email])
+    create_if_not_exists index(:rm_emails, [:bounce_count])
+
+    execute """
+            INSERT INTO rm_emails (id, email, inserted_at, updated_at)
+            SELECT uuid_generate_v4(), email, NOW(), NOW()
+            FROM user_teams
+            WHERE email IS NOT NULL
+            ON CONFLICT DO NOTHING
+            """,
+            "SELECT 1"
+
+    execute """
+            INSERT INTO rm_emails (id, email, inserted_at, updated_at)
+            SELECT uuid_generate_v4(), email, NOW(), NOW()
+            FROM user_leagues
+            WHERE email IS NOT NULL
+            ON CONFLICT DO NOTHING
+            """,
+            "SELECT 1"
+  end
+end
