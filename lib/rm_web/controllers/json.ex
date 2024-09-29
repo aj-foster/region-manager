d69038ee8bcdd5554f31429a0f083fdf84da5e73
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2024-09-28 23:24:52 -0400

    Add additional information from event proposals to API responses

--- lib/rm_web/controllers/json.ex
+++ lib/rm_web/controllers/json.ex
@@ -1,12 +1,30 @@
 defmodule RMWeb.JSON do
+  @typedoc "Generic API response"
+  @type response :: response(boolean, term, [term] | nil)
+  @typedoc "Generic API response"
+  @type response(success?, data, errors) :: %{success: success?, data: data, errors: errors}
+
+  @typedoc "Successful API response"
+  @type success :: success(term)
+  @typedoc "Successful API response"
+  @type success(data) :: response(true, data, nil)
+
+  @typedoc "Unsuccessful API response"
+  @type error :: error([term])
+  @typedoc "Unsuccessful API response"
+  @type error(errors) :: response(false, nil, errors)
+
+  @spec success(data) :: success(data) when data: term
   def success(data) do
     %{success: true, data: data, errors: nil}
   end
 
+  @spec error(error) :: error([error]) when error: term
   def error(error) do
     %{success: false, data: nil, errors: [error]}
   end
 
+  @spec errors(errors) :: error(errors) when errors: term
   def errors(errors) when is_list(errors) do
     %{success: false, data: nil, errors: errors}
   end
--- lib/rm_web/controllers/region_json.ex
+++ lib/rm_web/controllers/region_json.ex
@@ -1,6 +1,11 @@
 defmodule RMWeb.RegionJSON do
   use RMWeb, :json
 
+  #
+  # Region
+  #
+
+  @spec show(%{region: RM.FIRST.Region.t()}) :: RMWeb.JSON.success(map)
   def show(%{region: region}) do
     %RM.FIRST.Region{
       abbreviation: abbreviation,
@@ -33,22 +38,26 @@ defmodule RMWeb.RegionJSON do
     })
   end
 
+  #
+  # Events
+  #
+
+  @spec events(%{events: [RM.FIRST.Event.t()]}) :: RMWeb.JSON.success(map)
   def events(%{events: events}) do
     success(%{event_count: length(events), events: Enum.map(events, &event/1)})
   end
 
+  @spec event(RM.FIRST.Event.t()) :: map
   defp event(
          %RM.FIRST.Event{
            code: code,
            date_end: date_end,
            date_start: date_start,
            date_timezone: timezone,
-           live_stream_url: live_stream_url,
            name: name,
            region: region,
            season: season,
            type: type,
-           website: website,
            location: %{
              address: address,
              city: city,
@@ -62,12 +71,13 @@ defmodule RMWeb.RegionJSON do
       code: code,
       date_end: date_end,
       date_start: date_start,
+      description: event_description(event),
       format: RM.FIRST.Event.format_name(event),
-      live_stream_url: live_stream_url,
+      live_stream_url: event_live_stream_url(event),
       name: name,
       season: season,
       type: RM.FIRST.Event.type_name(type),
-      website: website,
+      website: event_website(event),
       league: event_league(event.league || event.local_league),
       location: %{
         name: venue,
@@ -85,6 +95,40 @@ defmodule RMWeb.RegionJSON do
     }
   end
 
+  @spec event_description(RM.FIRST.Event.t()) :: String.t() | nil
+  defp event_description(%RM.FIRST.Event{
+         proposal: %RM.Local.EventProposal{description: description}
+       }) do
+    description
+  end
+
+  defp event_description(_event), do: nil
+
+  @spec event_live_stream_url(RM.FIRST.Event.t()) :: String.t() | nil
+  defp event_live_stream_url(%RM.FIRST.Event{
+         proposal: %RM.Local.EventProposal{live_stream_url: live_stream_url}
+       }) do
+    live_stream_url
+  end
+
+  defp event_live_stream_url(%RM.FIRST.Event{live_stream_url: live_stream_url}) do
+    live_stream_url
+  end
+
+  @spec event_website(RM.FIRST.Event.t()) :: String.t() | nil
+  defp event_website(%RM.FIRST.Event{
+         proposal: %RM.Local.EventProposal{website: website}
+       }) do
+    website
+  end
+
+  defp event_website(%RM.FIRST.Event{website: website}) do
+    website
+  end
+
+  @spec event_league(nil) :: nil
+  @spec event_league(RM.FIRST.League.t()) :: map
+  @spec event_league(RM.Local.League.t()) :: map
   defp event_league(nil), do: nil
 
   defp event_league(%RM.FIRST.League{code: code, name: name, remote: remote, location: location}) do
@@ -95,6 +139,7 @@ defmodule RMWeb.RegionJSON do
     %{code: code, name: name, remote: remote, location: location}
   end
 
+  @spec event_registration(RM.FIRST.Event.t()) :: map | nil
   defp event_registration(
          %RM.FIRST.Event{
            region: region,
@@ -133,6 +178,7 @@ defmodule RMWeb.RegionJSON do
     nil
   end
 
+  @spec event_registration_open(RM.FIRST.Event.t()) :: boolean
   defp event_registration_open(
          %RM.FIRST.Event{
            settings: %RM.Local.EventSettings{
@@ -146,16 +192,26 @@ defmodule RMWeb.RegionJSON do
 
   defp event_registration_open(_event), do: false
 
+  @spec event_registration_attending_waitlist(RM.FIRST.Event.t()) ::
+          {[RM.Local.EventRegistration.t()], [RM.Local.EventRegistration.t()]}
   defp event_registration_attending_waitlist(%RM.FIRST.Event{registrations: registrations}) do
     registrations
     |> Enum.reject(& &1.rescinded)
     |> Enum.split_with(&(not &1.waitlisted))
   end
 
+  #
+  # Leagues
+  #
+
+  @spec leagues(%{leagues: [RM.FIRST.League.t() | RM.Local.League.t()]}) ::
+          RMWeb.JSON.success(map)
   def leagues(%{leagues: leagues}) do
     success(%{league_count: length(leagues), leagues: Enum.map(leagues, &league/1)})
   end
 
+  @spec league(RM.FIRST.League.t()) :: map
+  @spec league(RM.Local.League.t()) :: map
   defp league(%RM.FIRST.League{
          code: code,
          location: location,
@@ -189,10 +245,17 @@ defmodule RMWeb.RegionJSON do
     }
   end
 
+  #
+  # Teams
+  #
+
+  @spec teams(%{teams: [RM.FIRST.Team.t() | RM.Local.Team.t()]}) :: RMWeb.JSON.success(map)
   def teams(%{teams: teams}) do
     success(%{team_count: length(teams), teams: Enum.map(teams, &team/1)})
   end
 
+  @spec team(RM.FIRST.Team.t()) :: map
+  @spec team(RM.Local.Team.t()) :: map
   defp team(%RM.Local.Team{
          event_ready: event_ready,
          name: name,
@@ -236,6 +299,9 @@ defmodule RMWeb.RegionJSON do
     }
   end
 
+  @spec team_league(nil) :: nil
+  @spec team_league(RM.FIRST.League.t()) :: map
+  @spec team_league(RM.Local.League.t()) :: map
   defp team_league(nil), do: nil
 
   defp team_league(%RM.FIRST.League{code: code, location: location, name: name, remote: remote}) do
