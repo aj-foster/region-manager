ecf04751d73e0ab206aa937518c6a9716c865e83
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2025-01-04 00:28:45 -0500

    Add league to team listings

--- lib/rm_web/live/team_live/index.ex
+++ lib/rm_web/live/team_live/index.ex
@@ -151,9 +151,10 @@ defmodule RMWeb.TeamLive.Index do
         if first_league do
           RM.Repo.preload(first_league, :teams)
           |> Map.fetch!(:teams)
+          |> Enum.map(&Map.put(&1, :league, first_league))
           |> Enum.sort(&team_sort/2)
         else
-          RM.FIRST.list_teams_by_region(region, season: season)
+          RM.FIRST.list_teams_by_region(region, season: season, preload: [:league])
           |> Enum.sort(&team_sort/2)
         end
 
