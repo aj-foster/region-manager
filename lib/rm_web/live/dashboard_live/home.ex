ed432d14dea2311266e880e29a0ad3f7055752b4
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2026-05-27 23:01:34 -0400

    Remove active season indicator from home page

--- lib/rm_web/live/dashboard_live/home.ex
+++ lib/rm_web/live/dashboard_live/home.ex
@@ -23,8 +23,7 @@ defmodule RMWeb.DashboardLive.Home do
 
   @spec assign_seasons(Socket.t()) :: Socket.t()
   defp assign_seasons(socket) do
-    current = RM.System.current_season()
     seasons = RM.FIRST.list_seasons() |> Enum.reverse()
-    assign(socket, current_season: current, seasons: seasons, seasons_count: length(seasons))
+    assign(socket, seasons: seasons, seasons_count: length(seasons))
   end
 end
