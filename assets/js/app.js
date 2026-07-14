8461ef99a3992d21b91e2eb420cd02817b021aed
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2026-07-14 04:44:42 +0000

    Format JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,7 @@
-
-import "phoenix_html"
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import "phoenix_html";
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 
 //
 // Hooks
@@ -13,18 +12,18 @@ let Hooks = {};
 Hooks.DragDropStyle = {
   mounted() {
     this.el.addEventListener("dragover", () => {
-      this.el.dataset.drag = "active"
-    })
+      this.el.dataset.drag = "active";
+    });
 
     this.el.addEventListener("dragleave", () => {
-      delete this.el.dataset.drag
-    })
+      delete this.el.dataset.drag;
+    });
 
     this.el.addEventListener("drop", () => {
-      delete this.el.dataset.drag
-    })
-  }
-}
+      delete this.el.dataset.drag;
+    });
+  },
+};
 
 Hooks.AutoClearFlash = {
   mounted() {
@@ -51,45 +50,47 @@ Hooks.AutoClearFlash = {
 
     this.destroyed();
     this.mounted();
-  }
-}
+  },
+};
 
 //
 // Live Socket
 //
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
 let liveSocket = new LiveSocket("/live", Socket, {
   hooks: Hooks,
   params: {
     _csrf_token: csrfToken,
-    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
-  }
-})
+    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+  },
+});
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
+window.liveSocket = liveSocket;
 
 //
 // Events
 //
 
 // See RMWeb.Live.Util.push_js/3
-window.addEventListener("phx:js-exec", ({ detail: {to, attr} }) => {
-  document.querySelectorAll(to).forEach(el => {
-    liveSocket.execJS(el, el.getAttribute(attr))
-  })
-})
+window.addEventListener("phx:js-exec", ({ detail: { to, attr } }) => {
+  document.querySelectorAll(to).forEach((el) => {
+    liveSocket.execJS(el, el.getAttribute(attr));
+  });
+});
 
 // See RMWeb.Live.Util.copy/2
 window.addEventListener("phx:copy", (event) => {
@@ -100,7 +101,7 @@ window.addEventListener("phx:copy", (event) => {
   if (copyAttribute != null) {
     content = copyAttribute;
   } else if (event.target instanceof HTMLInputElement) {
-    content = event.target.value
+    content = event.target.value;
   } else {
     content = event.target.innerText;
   }
@@ -109,12 +110,12 @@ window.addEventListener("phx:copy", (event) => {
     const blob = new Blob([content], { contentType });
     const data = [new ClipboardItem({ [contentType]: blob })];
 
-    navigator.clipboard.write(data)
+    navigator.clipboard.write(data);
   } else {
-    navigator.clipboard.writeText(content)
+    navigator.clipboard.writeText(content);
   }
-})
+});
 
 window.addEventListener("phx:window-open", ({ detail: { url } }) => {
-  window.open(url, "_blank", "noopener noreferrer")
-})
+  window.open(url, "_blank", "noopener noreferrer");
+});
