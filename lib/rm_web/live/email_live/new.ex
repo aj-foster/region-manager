0664616dc78d7126306dd5024e4dbf89f69d8875
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2026-08-05 02:39:32 +0000

    Refactor more of the campaign logic to shared utils

--- lib/rm_web/live/email_live/new.ex
+++ lib/rm_web/live/email_live/new.ex
@@ -1,8 +1,6 @@
 defmodule RMWeb.EmailLive.New do
   use RMWeb, :live_view
-  import RMWeb.EmailLive.Util
-
-  alias RM.Email
+  import RMWeb.EmailLive.Util, only: [segment_options: 1, validate_params: 2]
 
   #
   # Lifecycle
@@ -10,13 +8,11 @@ defmodule RMWeb.EmailLive.New do
 
   on_mount {RMWeb.EmailLive.Util, :require_current_season}
   on_mount {RMWeb.EmailLive.Util, :require_permission}
+  on_mount {RMWeb.EmailLive.Util, :require_keila_records}
 
   @impl true
   def mount(_params, _session, socket) do
     socket
-    |> assign_project()
-    |> assign_segments()
-    |> assign_sender_id()
     |> assign_subject_prefix()
     |> new_email_form()
     |> ok()
@@ -44,42 +40,6 @@ defmodule RMWeb.EmailLive.New do
   #
   # Helpers
   #
-
-  @spec assign_project(Socket.t()) :: Socket.t()
-  defp assign_project(socket) do
-    season = socket.assigns[:season]
-    region = socket.assigns[:region]
-
-    if keila_project = Email.get_keila_project(region) do
-      assign(socket, :keila_project, keila_project)
-    else
-      socket
-      |> put_flash(
-        :error,
-        "No email project found for this region. Please contact an administrator."
-      )
-      |> push_navigate(to: url_for([season, region]))
-    end
-  end
-
-  @spec assign_sender_id(Socket.t()) :: Socket.t()
-  defp assign_sender_id(socket) do
-    league = socket.assigns[:local_league]
-    region = socket.assigns[:region]
-
-    if keila_sender_id = (league || region).metadata.keila_sender_id do
-      assign(socket, :keila_sender_id, keila_sender_id)
-    else
-      season = socket.assigns[:season]
-
-      socket
-      |> put_flash(
-        :error,
-        "No email sender found. Please contact an administrator."
-      )
-      |> push_navigate(to: url_for([season, region]))
-    end
-  end
 
   @spec assign_subject_prefix(Socket.t()) :: Socket.t()
   defp assign_subject_prefix(socket) do
@@ -128,41 +88,9 @@ defmodule RMWeb.EmailLive.New do
   defp prefix(%RM.Local.League{metadata: %{email_prefix: nil}}), do: nil
   defp prefix(%RM.Local.League{metadata: %{email_prefix: prefix}}), do: "[#{prefix}] "
 
-  @spec validate_params(Socket.t(), map) :: map
-  defp validate_params(socket, params) do
-    project_id = socket.assigns[:keila_project].id
-    segment_ids = Enum.map(socket.assigns[:keila_segments], fn {_, info} -> info.segment.id end)
-    sender_id = socket.assigns[:keila_sender_id]
-    template_id = socket.assigns[:region].metadata.keila_template_id
-
-    params = Map.put_new(params, "segment_id", List.first(segment_ids))
-
-    if params["segment_id"] not in segment_ids do
-      raise ArgumentError, "Invalid segment ID"
-    end
-
-    params
-    |> Map.merge(%{
-      "project_id" => project_id,
-      "public_link_enabled" => true,
-      "sender_id" => sender_id,
-      "template_id" => template_id,
-      "settings" => %{"type" => "markdown", "do_not_track" => true}
-    })
-  end
-
   #
   # Template Helpers
   #
-
-  @spec segment_options(map) :: [{String.t(), String.t()}]
-  defp segment_options(%{all: all_info, coach: coach_info, coach_ext: coach_ext_info}) do
-    [
-      {all_info.name <> " (#{all_info.count})", all_info.segment.id},
-      {coach_info.name <> " (#{coach_info.count})", coach_info.segment.id},
-      {coach_ext_info.name <> " (#{coach_ext_info.count})", coach_ext_info.segment.id}
-    ]
-  end
 
   @spec text_or_placeholder(String.t() | nil) :: String.t()
   defp text_or_placeholder(""), do: "_"
--- lib/rm_web/live/email_live/util.ex
+++ lib/rm_web/live/email_live/util.ex
@@ -39,9 +39,9 @@ defmodule RMWeb.EmailLive.Util do
   end
 
   def on_mount(:require_permission, _params, _session, socket) do
-    league = socket.assigns[:local_league]
-    region = socket.assigns[:region]
     season = socket.assigns[:season]
+    region = socket.assigns[:region]
+    league = socket.assigns[:local_league]
     user = socket.assigns[:current_user]
 
     if can?(user, :email_message_send, league || region) do
@@ -57,39 +57,50 @@ defmodule RMWeb.EmailLive.Util do
     end
   end
 
-  @doc "Assign available segments for the current league (if present) or region to the socket"
-  @spec assign_segments(Socket.t()) :: Socket.t()
-  def assign_segments(socket) do
-    keila_segments =
-      %{
-        all: get_segment(socket, :all),
-        coach: get_segment(socket, :coach),
-        coach_ext: get_segment(socket, :coach_ext)
-      }
-      |> Map.reject(fn {_key, value} -> is_nil(value) end)
+  def on_mount(:require_keila_records, _params, _session, socket) do
+    season = socket.assigns[:season]
+    region = socket.assigns[:region]
+    league = socket.assigns[:local_league]
 
-    if map_size(keila_segments) == 3 do
-      assign(socket, keila_segments: keila_segments)
-    else
-      season = socket.assigns[:season]
-      region = socket.assigns[:region]
-
+    with <<project_id::binary>> <- region.metadata.keila_project_id,
+         %{} = project <- RM.Email.get_keila_project(region),
+         <<segment_id::binary>> <- (league || region).metadata.keila_segment_id,
+         %{} = all_segment <- get_segment(project_id, segment_id, :all),
+         <<segment_id::binary>> <- (league || region).metadata.keila_coach_segment_id,
+         %{} = coach_segment <- get_segment(project_id, segment_id, :coach),
+         <<segment_id::binary>> <- (league || region).metadata.keila_extended_coach_segment_id,
+         %{} = extended_segment <- get_segment(project_id, segment_id, :coach_ext),
+         <<template_id::binary>> <- region.metadata.keila_template_id,
+         %{} = template <- Keila.Templates.get_project_template(project_id, template_id),
+         <<sender_id::binary>> <- (league || region).metadata.keila_sender_id,
+         %{} = sender <- Keila.Mailings.get_project_sender(project_id, sender_id) do
       socket
-      |> put_flash(
-        :error,
-        "One or more email segments are missing. Please contact an administrator."
+      |> assign(
+        keila_project: project,
+        keila_segments: %{
+          all: all_segment,
+          coach: coach_segment,
+          coach_ext: extended_segment
+        },
+        keila_sender: sender,
+        keila_template: template
       )
-      |> push_navigate(to: url_for([season, region]))
+      |> cont()
+    else
+      _ ->
+        socket
+        |> put_flash(
+          :error,
+          "Email setup is incomplete for this #{if league, do: "league", else: "region"}. Please contact an administrator."
+        )
+        |> push_navigate(to: url_for([season, region, league]))
+        |> halt()
     end
   end
 
-  @spec get_segment(Socket.t(), atom) :: {Keila.Contacts.Segment.t(), integer} | nil
-  defp get_segment(socket, segment_type) do
-    league = socket.assigns[:local_league]
-    region = socket.assigns[:region]
-    project_id = socket.assigns[:keila_project].id
-    segment_id = segment_id(league || region, segment_type)
-
+  @spec get_segment(Keila.Projects.Project.id(), Keila.Contacts.Segment.id(), atom) ::
+          {Keila.Contacts.Segment.t(), integer} | nil
+  defp get_segment(project_id, segment_id, segment_type) do
     if segment = Keila.Contacts.get_project_segment(project_id, segment_id) do
       segment_filter = segment.filter || %{}
       filter = %{"$and" => [segment_filter, %{"status" => "active"}]}
@@ -103,22 +114,43 @@ defmodule RMWeb.EmailLive.Util do
     end
   end
 
-  @doc "Extract the segment ID for a given segment type from a league or region"
-  @spec segment_id(RM.Local.League.t() | RM.FIRST.Region.t(), atom) :: String.t() | nil
-  def segment_id(league_or_region, segment_type) do
-    case {league_or_region, segment_type} do
-      {%RM.Local.League{metadata: %{keila_segment_id: id}}, :all} -> id
-      {%RM.Local.League{metadata: %{keila_coach_segment_id: id}}, :coach} -> id
-      {%RM.Local.League{metadata: %{keila_extended_coach_segment_id: id}}, :coach_ext} -> id
-      {%RM.FIRST.Region{metadata: %{keila_segment_id: id}}, :all} -> id
-      {%RM.FIRST.Region{metadata: %{keila_coach_segment_id: id}}, :coach} -> id
-      {%RM.FIRST.Region{metadata: %{keila_extended_coach_segment_id: id}}, :coach_ext} -> id
-    end
-  end
-
   @doc "Get a human-readable description for a given segment type"
   @spec segment_description(atom) :: String.t()
   def segment_description(:all), do: "Everyone subscribed to news"
   def segment_description(:coach), do: "Registered coaches"
   def segment_description(:coach_ext), do: "Registered coaches from this season and last season"
+
+  @doc "Validate campaign options and merge them with required fields"
+  @spec validate_params(Socket.t(), map) :: map
+  def validate_params(socket, params) do
+    segment_ids = Enum.map(socket.assigns[:keila_segments], fn {_, info} -> info.segment.id end)
+    params = Map.put_new(params, "segment_id", List.first(segment_ids))
+
+    if params["segment_id"] not in segment_ids do
+      raise ArgumentError, "Invalid segment ID"
+    end
+
+    params
+    |> Map.merge(%{
+      "project_id" => socket.assigns[:keila_project].id,
+      "public_link_enabled" => true,
+      "sender_id" => socket.assigns[:keila_sender].id,
+      "template_id" => socket.assigns[:keila_template].id,
+      "settings" => %{"type" => "markdown", "do_not_track" => true}
+    })
+  end
+
+  #
+  # Template Helpers
+  #
+
+  @doc "Create a list of options for a select input based on the given segments"
+  @spec segment_options(map) :: [{String.t(), String.t()}]
+  def segment_options(%{all: all_info, coach: coach_info, coach_ext: coach_ext_info}) do
+    [
+      {all_info.name <> " (#{all_info.count})", all_info.segment.id},
+      {coach_info.name <> " (#{coach_info.count})", coach_info.segment.id},
+      {coach_ext_info.name <> " (#{coach_ext_info.count})", coach_ext_info.segment.id}
+    ]
+  end
 end
