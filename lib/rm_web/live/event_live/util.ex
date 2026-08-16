07d17152c2ec56d0d821d44aed9fc4d3380d59b9
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2026-08-16 21:08:09 +0000

    Define event index navigation for published, proposals, and venues

--- lib/rm_web/live/event_live/util.ex
+++ lib/rm_web/live/event_live/util.ex
@@ -2,7 +2,61 @@ defmodule RMWeb.EventLive.Util do
   use RMWeb, :html
 
   @doc """
-  Unified navigation component for region-level views
+  Second-level navigation for event index views
+  """
+  attr :class, :string, default: nil, doc: "Additional classes for the navigation wrapper"
+  attr :league, :any, required: true, doc: "current league, `@local_league`"
+  attr :region, RM.FIRST.Region, required: true, doc: "current region, `@region`"
+  attr :season, :integer, required: true, doc: "current season, `@season`"
+  attr :user, RM.Account.User, default: nil, doc: "current user, `@current_user`"
+  attr :view, :any, required: true, doc: "`@socket.view`"
+
+  def event_index_nav(assigns) do
+    ~H"""
+    <div class={["flex font-title italic overflow-x-auto px-4 small-caps", @class]}>
+      <.sub_nav_item
+        current={@view}
+        navigate={
+          if @league,
+            do: ~p"/s/#{@season}/r/#{@region}/l/#{@league}/events",
+            else: ~p"/s/#{@season}/r/#{@region}/events"
+        }
+        target={RMWeb.EventLive.Index}
+      >
+        Published
+      </.sub_nav_item>
+      <.sub_nav_item
+        :if={@season == @region.current_season and can?(@user, :proposal_index, @region)}
+        children={[RMWeb.ProposalLive.New, RMWeb.ProposalLive.Show, RMWeb.ProposalLive.Edit]}
+        current={@view}
+        navigate={
+          if @league,
+            do: ~p"/s/#{@season}/r/#{@region}/l/#{@league}/proposals",
+            else: ~p"/s/#{@season}/r/#{@region}/proposals"
+        }
+        target={RMWeb.ProposalLive.Index}
+      >
+        Proposals
+      </.sub_nav_item>
+      <.sub_nav_item
+        :if={@season == @region.current_season and can?(@user, :venue_index, @region)}
+        children={[RMWeb.VenueLive.New, RMWeb.VenueLive.Show, RMWeb.VenueLive.Edit]}
+        current={@view}
+        navigate={
+          if @league,
+            do: ~p"/s/#{@season}/r/#{@region}/l/#{@league}/venues",
+            else: ~p"/s/#{@season}/r/#{@region}/venues"
+        }
+        target={RMWeb.VenueLive.Index}
+      >
+        Venues
+      </.sub_nav_item>
+    </div>
+    """
+  end
+
+  @doc """
+  Unified navigation component for event-level views
   """
   attr :class, :string, default: nil, doc: "Additional classes for the navigation wrapper"
   attr :event, RM.FIRST.Event, required: true, doc: "current event, `@event`"
@@ -14,7 +68,7 @@ defmodule RMWeb.EventLive.Util do
 
   def event_nav(assigns) do
     ~H"""
-    <.top_nav class="mb-8">
+    <.top_nav class={@class}>
       <.nav_item
         current={@view}
         navigate={
