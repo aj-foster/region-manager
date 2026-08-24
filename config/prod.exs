8c94fc3e51713ee2c4853a3306d5a7247240ec67
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2026-08-24 00:38:11 +0000

    Add AppSignal revision to deploys and reduce noise from API

--- config/prod.exs
+++ config/prod.exs
@@ -6,6 +6,17 @@ config :rm, RMWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.j
 # Dependencies
 #
 
+config :appsignal, :config,
+  # Capture revision as a short Git SHA at compile time
+  revision: System.get_env("APP_REVISION"),
+
+  # Sheer throughput from region websites would use all of AppSignal's requests
+  ignore_actions: [
+    "RMWeb.RegionController#events",
+    "RMWeb.RegionController#teams",
+    "RMWeb.RegionController#videos"
+  ]
+
 config :logger, level: :info
 
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: RM.Finch
